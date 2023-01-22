8042c9caf857d2c91fd4f95c598a85957b7d6586
ÁlvaroAlano <alvarob.alano@hotmail.com>
2023-01-22 18:36:11 -0300

    add readme

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const button = document.querySelector("header button")
 button.addEventListener("click", add)
 form.addEventListener("change", save)
 
-function add() {
+ function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
   if (dayExists) {
@@ -15,10 +15,12 @@ function add() {
   nlwSetup.addDay(today)
 }
 
+
+
 function save() {
   localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
-nlwSetup.load()
+nlwSetup.load() 
